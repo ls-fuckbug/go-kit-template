a0b3b72cffeb6e24fc21cad457fc9d1c6d6a8d2a
liushuai <liushuai@chuangxin.com>
2023-02-28 10:12:43 +0800

    index

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "hello world",
+		})
+	})
+
+	r.Run(":8989")
+}
