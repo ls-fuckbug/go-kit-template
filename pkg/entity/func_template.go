d331227670449d370bd364cc72260048f2ce0289
liushuai <liushuai@chuangxin.com>
2023-02-28 16:24:05 +0800

    update

--- pkg/entity/func_template.go
+++ pkg/entity/func_template.go
@@ -1,15 +1,110 @@
 package entity
 
 const (
-	BackendTemplateService = `
-func %s() {
-	fmt.Println("Hello, World!")
+	TemplateServiceImpl = `
+
+// %s 
+func (s svcImpl) %s(
+  ctx context.Context, 
+  req *pb.%sRequest,
+) (*pb.%sResponse, error) {
+
+}
+
+`
+	TemplateServiceInterface = `
+// %s 
+%s(ctx context.Context, req *pb.%sRequest) (*pb.%sResponse, error)
+`
+
+	BackendTemplateEndpointMake = `
+
+func make%sEndpoint(s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req, _ := request.(*entity.%sReq)
+		resp := s.%s(ctx, req)
+		return resp, nil
+	}
+}
+
+`
+	ServiceTemplateEndpointMake = `
+
+func make%sEndpoint(s service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req, _ := request.(*pb.%sReq)
+		resp := s.%s(ctx, req)
+		return resp, nil
+	}
+}
+
+`
+	TemplateEndpointMakeCall = `
+%s:         make%sEndpoint(svc),
+`
+	TemplateEndpointStructMember = `
+%s endpoint.Endpoint
+`
+	BackendTemplateTransportHandler = `
+	%sHandler := httptransport.NewServer(
+		endpoints.%s,
+		decodeHTTP%sRequest,
+		encodeHTTP%sResponse,
+		options...,
+	)
+`
+
+	ServiceTemplateTransportHandler = `
+	%sTransport := grpctransport.NewServer(
+		endpoints.%s,
+		decode%sRequest,
+		encode%sResponse,
+	)
+`
+
+	BackendTemplateTransportHandle = `
+	r.Handle("/", %sHandler).Methods("")
+`
+
+	ServiceTemplateTransportHandle = `
+		%s:         %sTransport,
+`
+	BackendTemplateTransportImpl = `
+
+func decodeHTTP%sRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request entity.%sReq
+	if e := json.NewDecoder(r.Body).Decode(&request); e != nil {
+		return nil, e
+	}
+	return &request, nil
+}
+
+func encodeHTTP%sResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	resp, _ := response.(*entity.%s)
+	middleware.LogAndAddRequestID(ctx, &resp.CommonRet)
+	return json.NewEncoder(w).Encode(resp)
 }
 `
 
-	ServiceTemplateService = `
-func %s() {
-	fmt.Println("Hello, World!")
+	ServiceTemplateTransportImpl = `
+
+func (n *grpcServer)%s(ctx context.Context,
+	req *pb.%sRequest) (*pb.%sResponse, error) {
+	_, resp, err := n.%s.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.%sResponse), nil
+}
+
+func decode%sRequest(_ context.Context, request interface{}) (interface{}, error) {
+	req, _ := request.(*pb.%sRequest)
+	return req, nil
+}
+
+func encode%sResponse(_ context.Context, response interface{}) (interface{}, error) {
+	resp, _ := response.(*pb.%sResponse)
+	return resp, nil
 }
 `
 )
--- pkg/service/generate_code.go
+++ pkg/service/generate_code.go
@@ -3,6 +3,7 @@ package service
 import (
 	"fmt"
 	"ls.com/kit/pkg/entity"
+	"unicode"
 )
 
 // Generate 定义处理函数
@@ -12,9 +13,9 @@ func Generate(funcName string, codeType string) (code string) {
 	} else {
 		switch codeType {
 		case "backend":
-			code = generateBackendTemplate(funcName)
+			code = generateBackendTemplate([]string{funcName})
 		case "service":
-			code = generateServiceTemplate(funcName)
+			code = generateServiceTemplate([]string{funcName})
 		default:
 			code = "不支持的codeType"
 		}
@@ -22,12 +23,107 @@ func Generate(funcName string, codeType string) (code string) {
 	}
 }
 
-func generateBackendTemplate(funcName string) (code string) {
-	code = fmt.Sprintf(entity.BackendTemplateService, funcName)
+// 首字母小写
+func uncapitalize(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	r := []rune(s)
+	r[0] = unicode.ToLower(r[0])
+	return string(r)
+}
+
+func generateBackendTemplate(funcName []string) (code string) {
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.TemplateServiceImpl, greatHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.TemplateServiceInterface, greatHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.BackendTemplateEndpointMake, greatHumpName, greatHumpName,
+			greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.TemplateEndpointMakeCall, greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		smallHumpName := uncapitalize(greatHumpName)
+		code += fmt.Sprintf(entity.BackendTemplateTransportHandler, smallHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		smallHumpName := uncapitalize(greatHumpName)
+		code += fmt.Sprintf(entity.BackendTemplateTransportHandle, smallHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.BackendTemplateTransportImpl, greatHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+
 	return
 }
 
-func generateServiceTemplate(funcName string) (code string) {
-	code = fmt.Sprintf(entity.ServiceTemplateService, funcName)
+func generateServiceTemplate(funcName []string) (code string) {
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.TemplateServiceImpl, greatHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.TemplateServiceInterface, greatHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.ServiceTemplateEndpointMake, greatHumpName, greatHumpName,
+			greatHumpName)
+	}
+	code += ""
+	for _, name := range funcName {
+		greatHumpName := name
+		code += fmt.Sprintf(entity.TemplateEndpointMakeCall, greatHumpName, greatHumpName)
+	}
+	code += ""
+	for _, name := range funcName {
+		greatHumpName := name
+		smallHumpName := uncapitalize(greatHumpName)
+		code += fmt.Sprintf(entity.ServiceTemplateTransportHandler, smallHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		smallHumpName := uncapitalize(greatHumpName)
+		code += fmt.Sprintf(entity.ServiceTemplateTransportHandle, smallHumpName, smallHumpName)
+	}
+	code += "$"
+	for _, name := range funcName {
+		greatHumpName := name
+		smallHumpName := uncapitalize(greatHumpName)
+		code += fmt.Sprintf(entity.ServiceTemplateTransportImpl, greatHumpName, greatHumpName,
+			greatHumpName, smallHumpName, greatHumpName, greatHumpName, greatHumpName,
+			greatHumpName, greatHumpName)
+	}
 	return
 }
